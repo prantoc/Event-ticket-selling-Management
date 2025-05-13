0d2597072d978f99d178ca2a6f003dd620988f98
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-13 18:50:33 +0600

    Refactor organizer routes to include authentication and media upload for profile creation and updates

--- backend/src/app/modules/Organizer/organizer.routes.js
+++ backend/src/app/modules/Organizer/organizer.routes.js
@@ -2,15 +2,31 @@ const express = require("express");
 const router = express.Router();
 const organizerController = require("./organizer.controller");
 const auth = require("../../middleware/auth");
+const {
+  uploadMedia,
+  setRelativePath,
+} = require("../../middleware/multerConfig");
 
 // Organizer routes (self)
-router.post("/",auth('user'), organizerController.createProfile);
-router.get("/me", organizerController.getProfile);
-router.put("/me", organizerController.updateProfile);
+router.post(
+  "/",
+  auth('superAdmin', 'admin',"user"),
+  uploadMedia.single("logo"),
+  setRelativePath,
+  organizerController.createProfile
+);
+router.get("/me", auth("user"), organizerController.getProfile);
+router.put(
+  "/me",
+  auth("user"),
+  uploadMedia.single("logo"),
+  setRelativePath,
+  organizerController.updateProfile
+);
 
 // Admin routes
-router.get("/", organizerController.getAllOrganizers);
-router.put("/:userId/approve", organizerController.approve);
-router.put("/:userId/reject", organizerController.reject);
-
+router.get("/", auth('superAdmin', 'admin'), organizerController.getAllOrganizers);
+router.put("/:userId/approve", auth('superAdmin', 'admin'), organizerController.approve);
+router.put("/:userId/reject", auth('superAdmin', 'admin'), organizerController.reject);
+router.delete("/:userId/delete",auth('superAdmin', 'admin') , organizerController.deleteOrganizer);
 module.exports = router;

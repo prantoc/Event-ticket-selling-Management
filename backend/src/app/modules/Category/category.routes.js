39e2ef488dca23dc6fea9c76129ed18c083249e4
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-14 10:51:00 +0600

    Refactor category routes to improve code readability and maintainability; add setRelativePath middleware for better file handling

--- backend/src/app/modules/Category/category.routes.js
+++ backend/src/app/modules/Category/category.routes.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const categoryController = require('./category.controller');
-const { uploadMedia } = require('../../middleware/multerConfig');
+const categoryController = require("./category.controller");
+const {
+  uploadMedia,
+  setRelativePath,
+} = require("../../middleware/multerConfig");
 
-router.post('/',uploadMedia.single("icon"), categoryController.createCategory);
-router.get('/', categoryController.getCategories);
-router.get('/:id', categoryController.getCategory);
-router.put('/:id', categoryController.updateCategory);
-router.delete('/:id', categoryController.deleteCategory);
+router.post(
+  "/",
+  uploadMedia.single("icon"),
+  setRelativePath,
+  categoryController.createCategory
+);
+router.get("/", categoryController.getCategories);
+router.get("/:id", categoryController.getCategory);
+router.put(
+  "/:id",
+  uploadMedia.single("icon"),
+  setRelativePath,
+  categoryController.updateCategory
+);
+router.delete("/:id", categoryController.deleteCategory);
 
 module.exports = router;

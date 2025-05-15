e09dd13bf58329b33c8dc9d798f9b828f96c63f6
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-15 17:21:03 +0600

    Refactor category routes to enforce authentication for all endpoints and add approval functionality

--- backend/src/app/modules/Category/category.routes.js
+++ backend/src/app/modules/Category/category.routes.js
@@ -5,21 +5,41 @@ const {
   uploadMedia,
   setRelativePath,
 } = require("../../middleware/multerConfig");
+const auth = require("../../middleware/auth");
 
 router.post(
   "/",
+  auth("superadmin", "admin", "user"),
   uploadMedia.single("icon"),
   setRelativePath,
   categoryController.createCategory
 );
-router.get("/", categoryController.getCategories);
-router.get("/:id", categoryController.getCategory);
+router.get(
+  "/",
+  auth("admin", "superadmin", "user"),
+  categoryController.getCategories
+);
+router.get(
+  "/:id",
+  auth("admin", "superadmin", "user"),
+  categoryController.getCategory
+);
 router.put(
   "/:id",
   uploadMedia.single("icon"),
   setRelativePath,
+  auth("admin", "superadmin"),
   categoryController.updateCategory
 );
-router.delete("/:id", categoryController.deleteCategory);
+router.put(
+  "/:id/approve",
+  auth("admin", "superadmin"),
+  categoryController.approveCategory
+);
+router.delete(
+  "/:id",
+  auth("admin", "superadmin"),
+  categoryController.deleteCategory
+);
 
 module.exports = router;

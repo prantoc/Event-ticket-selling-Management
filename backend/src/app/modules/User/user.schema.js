81afb85bfc9aef80a0a68d7d9331bf4a70c51f52
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-25 12:26:12 +0600

    Refactor user schema for consistent formatting and improved readability

--- backend/src/app/modules/User/user.schema.js
+++ backend/src/app/modules/User/user.schema.js
@@ -1,36 +1,45 @@
-
-const hashPassword = require('../../utils/hashedPassword');
-const mongoose = require('mongoose');
+const hashPassword = require("../../utils/hashedPassword");
+const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true, lowercase: true },
-        password: { type: String, required: true },
-        phone: { type: String, required: false },
-        address: { type: String, required: false },
-        role: { type: String, enum: ['superAdmin', 'admin', 'user','organizer'], required: false, default: 'user' },
-        isDeleted: { type: Boolean, default: false },
-        isVerified: { type: Boolean, default: false },
-        needsPasswordChange: {
-            type: Boolean,
-            default: true,
-        },
-        passwordChangedAt: {
-            type: Date,
-        },
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: false },
+    address: { type: String, required: false },
+    role: {
+      type: String,
+      enum: ["superAdmin", "admin", "user", "organizer"],
+      required: false,
+      default: "user",
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    }
+    isDeleted: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    passwordChangedAt: {
+      type: Date,
+    },
+    preferences: {
+      type: [String], 
+      required: false,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
-UserSchema.pre('save', async function (next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-    const hash = await hashPassword(user.password);
-    user.password = hash;
-    next();
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  const hash = await hashPassword(user.password);
+  user.password = hash;
+  next();
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);

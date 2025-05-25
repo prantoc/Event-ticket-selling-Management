6ed06f5b45dc4364ec4418796e06ecd852c83c1c
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-25 17:26:17 +0600

    Add payment schema for MongoDB model

---
+++ backend/src/app/modules/Payments/payment.schema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+  {
+    bookingId: { type: mongoose.Schema.Types.ObjectId, ref: "Booking" },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+    amount: Number,
+    currency: String,
+    paymentIntentId: String,
+    status: { type: String, enum: ["succeeded", "failed", "pending"] },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Payment", paymentSchema);

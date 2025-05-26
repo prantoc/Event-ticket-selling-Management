34b1480512fb6e0b601bd45825a158227f060d00
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-26 12:50:37 +0600

    Refactor webhook handling in app.js to streamline Stripe webhook integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
-const routes = require('./app/routes');
-const notFound = require('./app/middleware/notFound');
+const routes = require("./app/routes");
+const notFound = require("./app/middleware/notFound");
 // const globalErrorHandler = require('./app/middleware/globalErrorHandler');
-const globalErrorHandler = require('./app/middleware/globalError');
+const globalErrorHandler = require("./app/middleware/globalError");
 // app.use(cors());
-app.use(cors({ origin: '*' }));
-app.use("/api/v1/webhooks", require("./app/modules/Payments/payment.webhook.routes"));
+app.use(cors({ origin: "*" }));
+app.post(
+  "/api/v1/webhooks/stripe-webhook",
+  express.raw({ type: "application/json" }),
+  require("./app/modules/Payments/payment.webhook.controller")
+    .handleStripeWebhook
+);
 app.use(express.json());
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 app.use("/api/v1/local", express.static(path.join(__dirname, "app/local")));
 
-app.get('/', (req, res) => {
-    res.json({
-        success: true,
-        message: 'Welcome to the API',
-    });
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the API",
+  });
 });
 
 app.use(globalErrorHandler);

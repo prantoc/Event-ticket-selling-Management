6c99b5bc901badf594c2a3b0b320631d64f011dd
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-26 14:47:44 +0600

    testing web hook signature

--- backend/src/app/modules/Payments/payment.routes.js
+++ backend/src/app/modules/Payments/payment.routes.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const paymentController = require("./payment.controller");
 
 router.post("/create-checkout-session", paymentController.createCheckoutSession);
-
+// Stripe webhook route
+router.post(
+  "/stripe-webhook",
+  express.raw({ type: "application/json" }),
+  paymentController.handleStripeWebhook
+);
 module.exports = router;

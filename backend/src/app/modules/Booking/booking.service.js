fe7552b235aafe55db89b3bae8c574bfeed50167
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-26 15:43:42 +0600

    undefined payment details send

--- backend/src/app/modules/Booking/booking.service.js
+++ backend/src/app/modules/Booking/booking.service.js
@@ -185,7 +185,8 @@ exports.getBookingsByUserId = async (userId, query) => {
 
     // Hide ticketDetails if payment is not successful
     if (booking.paymentDetails?.status !== "succeeded") {
-      delete booking._doc.ticketDetails; // Ensure removal from the response object
+      // delete booking._doc.ticketDetails; // Ensure removal from the response object
+      booking.ticketDetails = undefined; 
     }
 
     return booking;

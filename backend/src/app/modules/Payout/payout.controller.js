ccc13cb6eee9c5a5773f5eb4b278afbf8d325f9e
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-27 11:14:21 +0600

    feat: implement payout controller with create, getAll, and update functionalities

---
+++ backend/src/app/modules/Payout/payout.controller.js
@@ -0,0 +1,34 @@
+const payoutService = require("./payout.service");
+
+exports.createPayout = async (req, res) => {
+  try {
+    const result = await payoutService.createPayout(req.body);
+    res
+      .status(201)
+      .json({ success: true, message: "Payout request created", data: result });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+exports.getAllPayouts = async (req, res) => {
+  try {
+    const result = await payoutService.getAllPayouts(req.query);
+    res
+      .status(200)
+      .json({ success: true, message: "Transaction fetched", data: result });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+exports.updatePayout = async (req, res) => {
+  try {
+    const result = await payoutService.updatePayout(req.params.id, req.body);
+    res
+      .status(200)
+      .json({ success: true, message: "Payout updated", data: result });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};

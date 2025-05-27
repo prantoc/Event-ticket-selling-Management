fbe5e0f1f1bf0a480edeef20b437c931f79b6a67
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-27 13:46:43 +0600

    fix: update payout logic to ensure accurate earnings calculations and prevent negative balances

--- backend/src/app/modules/Payout/payout.service.js
+++ backend/src/app/modules/Payout/payout.service.js
@@ -1,6 +1,33 @@
 const Payout = require("./payout.schema");
 const Organizer = require("../Organizer/organizer.schema");
-exports.createPayout = async (data) => {
+exports.createPayout = async (data, organizerId) => {
+  const organizer = await Organizer.findOne({
+    userId: organizerId,
+  });
+
+  if (!organizer) {
+    throw new Error("Organizer not found");
+  }
+
+  // Ensure earnings object exists
+  if (!organizer.earnings) {
+    organizer.earnings = {};
+  }
+
+  // Initialize fields if they don't exist
+  organizer.earnings.available = organizer.earnings.available || 0;
+  organizer.earnings.pending = organizer.earnings.pending || 0;
+
+  // Update earnings
+  organizer.earnings.available -= data.amount;
+  organizer.earnings.pending += data.amount;
+
+  // // Prevent negative available balance
+  if (organizer.earnings.available < 0) {
+    organizer.earnings.available = 0;
+  }
+
+  await organizer.save();
   const payout = new Payout(data);
   return await payout.save();
 };
@@ -33,16 +60,45 @@ exports.updatePayout = async (id, data) => {
     }
 
     // Initialize fields if they don't exist
-    organizer.earnings.available = organizer.earnings.available || 0;
+    organizer.earnings.pending = organizer.earnings.pending || 0;
     organizer.earnings.totalWithdraw = organizer.earnings.totalWithdraw || 0;
 
     // Update earnings
-    organizer.earnings.available -= updatedPayout.amount;
+    organizer.earnings.pending -= updatedPayout.amount;
     organizer.earnings.totalWithdraw += updatedPayout.amount;
 
     // // Prevent negative available balance
-    if (organizer.earnings.available < 0) {
-      organizer.earnings.available = 0;
+    if (organizer.earnings.pending < 0) {
+      organizer.earnings.pending = 0;
+    }
+
+    await organizer.save();
+  }
+  if (updatedPayout.status === "failed") {
+    const organizer = await Organizer.findOne({
+      userId: updatedPayout.organizerId,
+    });
+
+    if (!organizer) {
+      throw new Error("Organizer not found");
+    }
+
+    // Ensure earnings object exists
+    if (!organizer.earnings) {
+      organizer.earnings = {};
+    }
+
+    // Initialize fields if they don't exist
+    organizer.earnings.pending = organizer.earnings.pending || 0;
+    organizer.earnings.available = organizer.earnings.available || 0;
+
+    // Update earnings
+    organizer.earnings.pending -= updatedPayout.amount;
+    organizer.earnings.available += updatedPayout.amount;
+
+    // // Prevent negative available balance
+    if (organizer.earnings.pending < 0) {
+      organizer.earnings.pending = 0;
     }
 
     await organizer.save();

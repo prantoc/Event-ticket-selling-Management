a68b06006919126828b5a093c065f4f8e70b12fa
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-29 15:51:41 +0600

    feat: add additional image fields and social media links to settings schema

--- backend/src/app/modules/Settings/settings.schema.js
+++ backend/src/app/modules/Settings/settings.schema.js
@@ -1,21 +1,37 @@
 // models/Setting.js
 const mongoose = require("mongoose");
 
-const settingSchema = new mongoose.Schema({
-  globalCommissionRate: { type: Number, default: 5 },
-  organizerCommissionRates: [
-    {
-      organizerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      commissionRate: Number,
-    },
-  ],
-  companyName: String,
-  currencySymbol: String,
-  taxAlias: String,
-  taxPercentage: Number,
-  daysBeforeProductExpiry: Number,
-  address: String,
-  companyLogo: String,
-}, { timestamps: true });
+const settingSchema = new mongoose.Schema(
+  {
+    globalCommissionRate: { type: Number, default: 5 },
+    organizerCommissionRates: [
+      {
+        organizerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        commissionRate: Number,
+      },
+    ],
+    companyName: String,
+    currencySymbol: String,
+    taxAlias: String,
+    taxPercentage: Number,
+    daysBeforeProductExpiry: Number,
+    address: String,
+    companyLogo: String,
+
+    infoFirstImage: String,
+    infoSecondImage: String,
+    marqueeImage: String,
+
+    facebook: String,
+    twitter: String,
+    instagram: String,
+    linkedin: String,
+    youtube: String,
+    whatsapp: String,
+    email: String,
+    phone: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Setting", settingSchema);

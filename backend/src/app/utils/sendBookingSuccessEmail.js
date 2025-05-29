1eccaa6b0b2e67c8006caccf45663b881dc2ee21
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-05-29 18:28:10 +0600

    fix: remove title from email confirmation template

--- backend/src/app/utils/sendBookingSuccessEmail.js
+++ backend/src/app/utils/sendBookingSuccessEmail.js
@@ -14,7 +14,6 @@ const sendBookingSuccessEmail = (userEmail, eventName,amount) => {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Confirm Your Email</title>
       <style>
         body {
           font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;

3309bf841aef7844d366d7991cdddaea36f73e0d
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2025-06-02 16:49:36 +0600

    feat: add allowedCountries utility module with supported country codes

---
+++ backend/src/app/utils/allowedCountries.js
@@ -0,0 +1,6 @@
+module.exports = [
+  'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE',
+  'ES', 'FI', 'FR', 'GR', 'HR', 'HU', 'IE', 'IT',
+  'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO',
+  'SE', 'SI', 'SK', 'NO', 'IS', 'LI', 'CH', 'GB'
+];
